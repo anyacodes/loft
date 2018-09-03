4ec9d6a1dd457602d263731d9d2de5b4b9e3cf5a
annademidova <ad3110@columbia.edu>
2018-09-03 09:16:27 -0400

    final hw6

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@
 /*
  Задание 1:
 
- Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
+ Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунд
 
  Пример:
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+    seconds = 1000;
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve();
+        }, seconds);
+    });
 }
-
 /*
  Задание 2:
 
@@ -25,6 +30,26 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.responseType = 'json';
+        xhr.addEventListener('load', () => {
+            resolve(xhr.response.sort(function (a, b) {
+                if (a.name > b.name) {
+
+                    return 1;
+                }
+                if (a.name < b.name) {
+
+                    return -1;
+                }
+
+                return 0;
+            }));
+        });
+        xhr.send();
+    });
 }
 
 export {
